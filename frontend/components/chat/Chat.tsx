0a987bbd08294c7b503c50ad6f2618ce13bbcb50
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2025-03-27 22:05:37 +1000

    chore: refactor out common chat providers

--- frontend/components/chat/Chat.tsx
+++ frontend/components/chat/Chat.tsx
@@ -1,15 +1,9 @@
 "use client"
 
 import { useState } from "react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { SocketProvider } from '@/context/SocketContext'
 import { ChatWindow } from "./ChatWindow"
 import { ChatFab } from "./FAB"
-import { AuthProvider } from "@/context/AuthContext"
-import { TooltipProvider } from "@radix-ui/react-tooltip"
-import { Toaster } from "sonner"
-
-const queryClient = new QueryClient()
+import { ChatProviders } from "./ChatProviders"
 
 type ChatProps = {
   mode?: 'single-threaded' | 'multi-threaded'
@@ -23,22 +17,17 @@ export function Chat({ mode = 'multi-threaded' }: ChatProps) {
   }
 
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <SocketProvider>
-          <TooltipProvider>
-            <div className="fixed z-50 bottom-4 right-4">
-              {isOpen ? (
-                <ChatWindow toggleChat={toggleChat} mode={mode} />
-              ) : (
-                <ChatFab onClick={toggleChat} />
-              )}
-              <Toaster position="top-right" richColors />
-            </div>
-          </TooltipProvider>
-        </SocketProvider>
-      </QueryClientProvider>
-    </AuthProvider>
+    <ChatProviders>
+      <div className="fixed z-50 bottom-4 right-4">
+        <div className="fixed z-50 bottom-4 right-4">
+          {isOpen ? (
+            <ChatWindow toggleChat={toggleChat} mode={mode} />
+          ) : (
+            <ChatFab onClick={toggleChat} />
+          )}
+        </div>
+      </div>
+    </ChatProviders>
   )
 }
 
---
+++ frontend/components/chat/ChatProviders.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { SocketProvider } from '@/context/SocketContext'
+import { AuthProvider } from "@/context/AuthContext"
+import { TooltipProvider } from "@radix-ui/react-tooltip"
+import { ReactNode } from "react"
+import { Toaster } from "sonner"
+
+const queryClient = new QueryClient()
+
+type ChatProvidersProps = {
+    children: ReactNode
+}
+
+export function ChatProviders({ children }: ChatProvidersProps) {
+    return (
+        <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <SocketProvider>
+                    <TooltipProvider>
+                        {children}
+                        <Toaster position="top-right" richColors />
+                    </TooltipProvider>
+                </SocketProvider>
+            </QueryClientProvider>
+        </AuthProvider>
+    )
+} 
